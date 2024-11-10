1015d1a812fdb43bb0a4c0697183f0bac8dd6a1b
Boris Zakatov <36029042+audi20113@users.noreply.github.com>
2024-11-10 23:20:43 +0300

    Delete courses_202411102130.sql

--- courses_202411102130.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public.courses ("name",is_exam,min_grade,max_grade) VALUES
-	 ('Математика',true,50,100),
-	 ('Физика',true,40,100),
-	 ('История',false,60,100),
-	 ('Информатика',true,70,100);
