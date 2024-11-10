da4e4ff4308a45d9a965a6ffa5a42abf0180ac3e
Boris Zakatov <36029042+audi20113@users.noreply.github.com>
2024-11-10 23:21:12 +0300

    Delete _SELECT_g_full_name_COUNT_s_id_AS_student_count_FROM_groups_g_LE_202411102131.sql

--- _SELECT_g_full_name_COUNT_s_id_AS_student_count_FROM_groups_g_LE_202411102131.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "SELECT g.full_name, COUNT(s.id) AS student_count
-FROM groups g
-LEFT JOIN students s ON g.id = s.group_id
-GROUP BY g.full_name
-" (full_name,student_count) VALUES
-	 ('Группа А',3),
-	 ('Группа Б',2);
