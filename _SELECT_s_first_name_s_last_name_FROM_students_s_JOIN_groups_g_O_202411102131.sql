de8df97c997d39cb6f72987db9b2af881eecc904
Boris Zakatov <36029042+audi20113@users.noreply.github.com>
2024-11-10 23:21:07 +0300

    Delete _SELECT_s_first_name_s_last_name_FROM_students_s_JOIN_groups_g_O_202411102131.sql

--- _SELECT_s_first_name_s_last_name_FROM_students_s_JOIN_groups_g_O_202411102131.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO "SELECT s.first_name, s.last_name
-FROM students s
-JOIN groups g ON s.group_id = g.id
-WHERE g.short_name = 'НаучБ'" (first_name,last_name) VALUES
-	 ('Екатерина','Кузнецова'),
-	 ('Дмитрий','Соколов');
